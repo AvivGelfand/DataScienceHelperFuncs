77273f22e1426d187c2483f8c25d8db3b170775f
AvivGelfand <63909805+AvivGelfand@users.noreply.github.com>
2023-04-27 04:00:04 +0300

    find_high_correlations

--- helpers.py
+++ helpers.py
@@ -7,6 +7,13 @@ from scipy.stats import f_oneway  # for ANOVA
 from sklearn.feature_selection import chi2  # for chi2
 from sklearn.preprocessing import StandardScaler
 
+def find_high_correlations(df, threshold):
+    corr_mat = df.corr().unstack().sort_values(kind="quicksort").drop_duplicates()
+    high_corr = corr_mat[corr_mat >= threshold].reset_index()
+    high_corr.columns = ["feature_1", "feature_2", "correlation"]
+    high_corr = high_corr[high_corr["feature_1"] != high_corr["feature_2"]]
+    return list(high_corr[["feature_1", "feature_2"]].to_records(index=False))
+
 
 def show_distribution(df, col):
     # creating two subplots:
